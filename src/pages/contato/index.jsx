54d7bb103ae265fa131552a859dee06a3a0d5115
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-05-16 22:41:08 -0300

    Refactor pages structure

--- src/pages/contato/index.jsx
+++ src/pages/contato/index.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ContainerPage } from "../../components/ContainerPage";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { MainContact } from "../../components/MainContact";
+import { MainContact } from "../../components/MainPages/MainContact";
 
 export const Contato = () => {
   return (
--- src/pages/projetos/index.jsx
+++ src/pages/projetos/index.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { ContainerPage } from "../../components/ContainerPage";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { MainProjects } from "../../components/MainProjects";
+import { MainProjects } from "../../components/MainPages/MainProjects";
 
 export const Projetos = () => {
   return (
