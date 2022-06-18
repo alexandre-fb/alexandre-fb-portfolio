8567e2c247de2fbd0a1e83f3c3b46424c6fed8c9
Alexandre Bisognin <alexandrefb.86@gmail.com>
2022-06-18 10:50:29 -0300

    Change profile image

--- src/components/profileData/ProfileImage/index.jsx
+++ src/components/profileData/ProfileImage/index.jsx
@@ -1,12 +1,12 @@
 import { Img } from "./styles";
-
+import profileImage from "../../../assets/images/profile-image.jpg"
 export const ProfileImage = () => {
   return (
     <Img
       layoutId="profile-image"
       transition={{ duration: 0.7 }}
-      src="..\src\assets\images\profile-image.jpg"
-      alt="Profile Image"
+      src={profileImage}
+      alt="Alexandre Bisognin"
     ></Img>
   );
 };
